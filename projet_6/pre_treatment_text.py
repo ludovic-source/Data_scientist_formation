835599713e2c6faa729a30d9c506dc5c59130f38
ludovic-source <ludovic.ch.lc@gmail.com>
2025-01-15 12:18:25 +0100

    Update pre_treatment_text.py

--- projet_6/pre_treatment_text.py
+++ projet_6/pre_treatment_text.py
@@ -19,6 +19,8 @@ from wordcloud import WordCloud
 from collections import defaultdict
 from collections import Counter
 
+import re
+
 # ----------------------------------------------------------------------------------------------------------------------------
 
 def freq_stats_from_dataframe(df):
@@ -201,74 +203,6 @@ def process_text(df, sw):
 
 # ----------------------------------------------------------------------------------------------------------------------------
 
-def process_text_2(df, sw):
-    """
-    Processes text data from a DataFrame, calculates word frequencies, and removes stopwords 
-    while lemmatizing the words. It also filters out non-English words using a provided 
-    dictionary of English words.
-
-    This function takes a DataFrame with two columns, 'product_name' and 'description', 
-    and applies the following steps:
-    - Tokenizes the text from the 'description' column.
-    - Lemmatizes each word in the description.
-    - Removes stopwords (from the 'sw' set) and non-English words (not in 'english_words').
-    - Removes words with only one character.
-    - Calculates the word frequencies for each 'product_name' in the DataFrame.
-
-    Args:
-        df (pd.DataFrame): A DataFrame with two columns:
-            - 'product_name': The name of the product.
-            - 'description': The description of the product.
-        sw (set): A set of stopwords to exclude from the word list.
-
-    Returns:
-        freq (dict): A dictionary where the keys are product names and the values are 
-            frequency distributions of the words in the descriptions.
-        stats (pd.DataFrame): A DataFrame with statistics for each product, 
-            including total word count and unique word count.
-        corpora (defaultdict): A dictionary where the keys are product names and the values 
-            are lists of words from the descriptions after lemmatization, stopword removal, 
-            and English word filtering.
-    """
-
-    # Initialiser le lemmatizer
-    lemmatizer = WordNetLemmatizer()
-
-    # tokenizer pour conserver uniquement les caractères alphanumériques
-    tokenizer = nltk.RegexpTokenizer(r'\w+')
-
-    # Télécharger le dictionnaire de mots si vous ne l'avez pas encore fait
-    nltk.download('words')
-
-    # Liste des mots valides en anglais (dictionnaire de NLTK)
-    valid_words = set(words.words())
-    
-    corpora = defaultdict(list)
-    
-    # Construction du corpus par image
-    for _, row in df.iterrows():
-        product = row['product_name']
-        description = row['description']
-        
-        # Tokenisation de la description
-        tokens = tokenizer.tokenize(description.lower())
-        # Lemmatisation des mots (tokens)
-        lemmatized_tokens = [lemmatizer.lemmatize(w) for w in tokens]
-        # Suppression des stopwords (sw) et des mots qui ne sont pas dans le dictionnaire anglais,
-        # et des mots d'un seul caractère
-        corpora[product] += [w for w in lemmatized_tokens if w not in sw and w in valid_words and len(w) > 1]
-    
-    # Calcul des fréquences et des statistiques
-    freq = {product: nltk.FreqDist(words) for product, words in corpora.items()}
-    stats = {product: {'total': len(words), 'unique': len(nltk.FreqDist(words).keys())} for product, words in corpora.items()}
-    
-    # Conversion des statistiques en DataFrame
-    stats_df = pd.DataFrame.from_dict(stats, orient='index')
-    
-    return freq, stats_df, corpora
-
-# ----------------------------------------------------------------------------------------------------------------------------
-
 # Obtenir le classement des mots les plus présents
 def get_most_common_words(freq):
     """
@@ -336,4 +270,28 @@ def print_wordcloud(words):
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis('off')
     plt.show()
-    
+
+# ----------------------------------------------------------------------------------------------------------------------------
+
+def clean_category_first_level(category):
+
+    """
+    Extract the first-level category from a category string and clean it.
+
+    This function takes a category string, extracts the portion before the
+    first occurrence of '>>', and retains only alphanumeric characters and spaces.
+
+    Args:
+        category (str): The category string containing hierarchical levels.
+
+    Returns:
+        str: The cleaned first-level category as a string.
+    """
+
+    # Sélectionner la catégorie de premier niveau
+    cat_first_level = category.split(">>")[0].strip()
+
+    # Conserver uniquement les caractères alphanumériques et les espaces
+    result = re.sub(r'[^a-zA-Z0-9 ]', '', cat_first_level)
+
+    return result
